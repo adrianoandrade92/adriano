103835228acf53ecac5cad4ef64fade307eeafad
Adriano <adrianoandrade92@gmail.com>
2022-09-20 00:45:03 -0300

    CSS config 1

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,26 @@
 //git add -A
 //git commit -m 'primeira página' 
 //git push origin main , empurra p o github
+//npm run dev desenvolver localmente
 
 import React, { useState } from "react"
 
 //Aqui começa front end//
 function Home() {
-    return <div>        
-        <h1>Adriano Andrade Marques</h1>
-        
+
+    return <div>
         <Contador />
+        <Curriculo_html>Aqui</Curriculo_html>
+             
+        <h1>Adriano Andrade Marques</h1>
+                     
         <div>
             <h1>Bruna, você é a esposa mais linda do mundo</h1>
             <h3>ASS: Adriano Andrade</h3>
+            <link href="Curriculo_html.html"></link> 
         </div>
+
+
     </div>
 }
 //Aqui acaba front end//
